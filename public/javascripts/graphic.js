8b1b6bd9dd425da7cb6450a8785896b81ec78c8a
Konrad Eichstaedt <konrad.eichstaedt@gmx.de>
2021-10-10 11:29:40 +0200

    Refactoring programm for separation of concerns

--- public/javascripts/graphic.js
+++ public/javascripts/graphic.js
@@ -11,432 +11,46 @@ function compileShadder(context,type, source) {
     return vs;
 }
 
+function linkProgramm(context,vertextSource,fragmentSource) {
+    var prog = context.createProgram();
+    context.attachShader(prog, compileShadder(context,context.VERTEX_SHADER,vertextSource));
+    context.attachShader(prog, compileShadder(context,context.FRAGMENT_SHADER,fragmentSource));
+    context.linkProgram(prog);
+    context.useProgram(prog);
+
+    return prog;
+}
+
+function bindVertex(context,program){
+    var posAttrib = context.getAttribLocation(program, 'pos');
+    context.vertexAttribPointer(posAttrib, 2, gl.FLOAT, false, 0, 0);
+    context.enableVertexAttribArray(posAttrib);
+}
+
+function loadVertexData(context,data) {
+    var vbo = context.createBuffer();
+    context.bindBuffer(context.ARRAY_BUFFER, vbo);
+    context.bufferData(context.ARRAY_BUFFER, data, context.STATIC_DRAW);
+}
+
+function clearContextAndDraw(context) {
+    context.clear(gl.COLOR_BUFFER_BIT);
+    context.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+}
+
 const vertexSource = 'attribute vec2 pos;void main(){gl_Position = vec4(pos * 0.5, 0, 1); }'
 const fragmentSource = 'void main() { gl_FragColor = vec4(1); }';
+
 const gl = createWebGLContext('c')
+
 gl.clearColor(0, 0, 0, 1);
 
-            // Link together into a program
-            var prog = gl.createProgram();
-            gl.attachShader(prog, compileShadder(gl,gl.VERTEX_SHADER,vertexSource));
-            gl.attachShader(prog, compileShadder(gl,gl.FRAGMENT_SHADER,fragmentSource));
-            gl.linkProgram(prog);
-            gl.useProgram(prog);
+var prog = linkProgramm(gl,vertexSource,fragmentSource)
 
-            // Load vertex data into a buffer
-            var vertices = new Float32Array([1, 1, -1, 1, 1, -1, -1, -1]);
-            var vbo = gl.createBuffer();
-            gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-            gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+var vertices = new Float32Array([1, 1, -1, 1, 1, -1, -1, -1]);
 
-            // Bind vertex buffer to attribute variable
-            var posAttrib = gl.getAttribLocation(prog, 'pos');
-            gl.vertexAttribPointer(posAttrib, 2, gl.FLOAT, false, 0, 0);
-            gl.enableVertexAttribArray(posAttrib);
+loadVertexData(gl,vertices)
 
-            // Clear framebuffer and render primitives
-            var vsSource = 'attribute vec2 pos;'+
-                'void main(){gl_Position = vec4(pos * 0.5, 0, 1); }';
-            var vs = gl.createShader(gl.VERTEX_SHADER);
-            gl.shaderSource(vs, vsSource);
-            gl.compileShader(vs);
+bindVertex(gl,prog)
 
-            // Compile a fragment shader
-            fsSouce =  'void main() { gl_FragColor = vec4(1); }';
-            var fs = gl.createShader(gl.FRAGMENT_SHADER);
-            gl.shaderSource(fs, fsSouce);
-            gl.compileShader(fs);
-
-            // Link together into a program
-            var prog = gl.createProgram();
-            gl.attachShader(prog, vs);
-            gl.attachShader(prog, fs);
-            gl.linkProgram(prog);
-            gl.useProgram(prog);
-
-            // Load vertex data into a buffer
-            var vertices = new Float32Array([1, 1, -1, 1, 1, -1, -1, -1]);
-            var vbo = gl.createBuffer();
-            gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-            gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-
-            // Bind vertex buffer to attribute variable
-            var posAttrib = gl.getAttribLocation(prog, 'pos');
-            gl.vertexAttribPointer(posAttrib, 2, gl.FLOAT, false, 0, 0);
-            gl.enableVertexAttribArray(posAttrib);
-
-            // Clear framebuffer and render primitives
-            var vsSource = 'attribute vec2 pos;'+
-                'void main(){gl_Position = vec4(pos * 0.5, 0, 1); }';
-            var vs = gl.createShader(gl.VERTEX_SHADER);
-            gl.shaderSource(vs, vsSource);
-            gl.compileShader(vs);
-
-            // Compile a fragment shader
-            fsSouce =  'void main() { gl_FragColor = vec4(1); }';
-            var fs = gl.createShader(gl.FRAGMENT_SHADER);
-            gl.shaderSource(fs, fsSouce);
-            gl.compileShader(fs);
-
-            // Link together into a program
-            var prog = gl.createProgram();
-            gl.attachShader(prog, vs);
-            gl.attachShader(prog, fs);
-            gl.linkProgram(prog);
-            gl.useProgram(prog);
-
-            // Load vertex data into a buffer
-            var vertices = new Float32Array([1, 1, -1, 1, 1, -1, -1, -1]);
-            var vbo = gl.createBuffer();
-            gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-            gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-
-            // Bind vertex buffer to attribute variable
-            var posAttrib = gl.getAttribLocation(prog, 'pos');
-            gl.vertexAttribPointer(posAttrib, 2, gl.FLOAT, false, 0, 0);
-            gl.enableVertexAttribArray(posAttrib);
-
-            // Clear framebuffer and render primitives
-            var vsSource = 'attribute vec2 pos;'+
-                'void main(){gl_Position = vec4(pos * 0.5, 0, 1); }';
-            var vs = gl.createShader(gl.VERTEX_SHADER);
-            gl.shaderSource(vs, vsSource);
-            gl.compileShader(vs);
-
-            // Compile a fragment shader
-            fsSouce =  'void main() { gl_FragColor = vec4(1); }';
-            var fs = gl.createShader(gl.FRAGMENT_SHADER);
-            gl.shaderSource(fs, fsSouce);
-            gl.compileShader(fs);
-
-            // Link together into a program
-            var prog = gl.createProgram();
-            gl.attachShader(prog, vs);
-            gl.attachShader(prog, fs);
-            gl.linkProgram(prog);
-            gl.useProgram(prog);
-
-            // Load vertex data into a buffer
-            var vertices = new Float32Array([1, 1, -1, 1, 1, -1, -1, -1]);
-            var vbo = gl.createBuffer();
-            gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-            gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-
-            // Bind vertex buffer to attribute variable
-            var posAttrib = gl.getAttribLocation(prog, 'pos');
-            gl.vertexAttribPointer(posAttrib, 2, gl.FLOAT, false, 0, 0);
-            gl.enableVertexAttribArray(posAttrib);
-
-            // Clear framebuffer and render primitives
-            var vsSource = 'attribute vec2 pos;'+
-                'void main(){gl_Position = vec4(pos * 0.5, 0, 1); }';
-            var vs = gl.createShader(gl.VERTEX_SHADER);
-            gl.shaderSource(vs, vsSource);
-            gl.compileShader(vs);
-
-            // Compile a fragment shader
-            fsSouce =  'void main() { gl_FragColor = vec4(1); }';
-            var fs = gl.createShader(gl.FRAGMENT_SHADER);
-            gl.shaderSource(fs, fsSouce);
-            gl.compileShader(fs);
-
-            // Link together into a program
-            var prog = gl.createProgram();
-            gl.attachShader(prog, vs);
-            gl.attachShader(prog, fs);
-            gl.linkProgram(prog);
-            gl.useProgram(prog);
-
-            // Load vertex data into a buffer
-            var vertices = new Float32Array([1, 1, -1, 1, 1, -1, -1, -1]);
-            var vbo = gl.createBuffer();
-            gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-            gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-
-            // Bind vertex buffer to attribute variable
-            var posAttrib = gl.getAttribLocation(prog, 'pos');
-            gl.vertexAttribPointer(posAttrib, 2, gl.FLOAT, false, 0, 0);
-            gl.enableVertexAttribArray(posAttrib);
-
-            // Clear framebuffer and render primitives
-            var vsSource = 'attribute vec2 pos;'+
-                'void main(){gl_Position = vec4(pos * 0.5, 0, 1); }';
-            var vs = gl.createShader(gl.VERTEX_SHADER);
-            gl.shaderSource(vs, vsSource);
-            gl.compileShader(vs);
-
-            // Compile a fragment shader
-            fsSouce =  'void main() { gl_FragColor = vec4(1); }';
-            var fs = gl.createShader(gl.FRAGMENT_SHADER);
-            gl.shaderSource(fs, fsSouce);
-            gl.compileShader(fs);
-
-            // Link together into a program
-            var prog = gl.createProgram();
-            gl.attachShader(prog, vs);
-            gl.attachShader(prog, fs);
-            gl.linkProgram(prog);
-            gl.useProgram(prog);
-
-            // Load vertex data into a buffer
-            var vertices = new Float32Array([1, 1, -1, 1, 1, -1, -1, -1]);
-            var vbo = gl.createBuffer();
-            gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-            gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-
-            // Bind vertex buffer to attribute variable
-            var posAttrib = gl.getAttribLocation(prog, 'pos');
-            gl.vertexAttribPointer(posAttrib, 2, gl.FLOAT, false, 0, 0);
-            gl.enableVertexAttribArray(posAttrib);
-
-            // Clear framebuffer and render primitives
-            var vsSource = 'attribute vec2 pos;'+
-                'void main(){gl_Position = vec4(pos * 0.5, 0, 1); }';
-            var vs = gl.createShader(gl.VERTEX_SHADER);
-            gl.shaderSource(vs, vsSource);
-            gl.compileShader(vs);
-
-            // Compile a fragment shader
-            fsSouce =  'void main() { gl_FragColor = vec4(1); }';
-            var fs = gl.createShader(gl.FRAGMENT_SHADER);
-            gl.shaderSource(fs, fsSouce);
-            gl.compileShader(fs);
-
-            // Link together into a program
-            var prog = gl.createProgram();
-            gl.attachShader(prog, vs);
-            gl.attachShader(prog, fs);
-            gl.linkProgram(prog);
-            gl.useProgram(prog);
-
-            // Load vertex data into a buffer
-            var vertices = new Float32Array([1, 1, -1, 1, 1, -1, -1, -1]);
-            var vbo = gl.createBuffer();
-            gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-            gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-
-            // Bind vertex buffer to attribute variable
-            var posAttrib = gl.getAttribLocation(prog, 'pos');
-            gl.vertexAttribPointer(posAttrib, 2, gl.FLOAT, false, 0, 0);
-            gl.enableVertexAttribArray(posAttrib);
-
-            // Clear framebuffer and render primitives
-            var vsSource = 'attribute vec2 pos;'+
-                'void main(){gl_Position = vec4(pos * 0.5, 0, 1); }';
-            var vs = gl.createShader(gl.VERTEX_SHADER);
-            gl.shaderSource(vs, vsSource);
-            gl.compileShader(vs);
-
-            // Compile a fragment shader
-            fsSouce =  'void main() { gl_FragColor = vec4(1); }';
-            var fs = gl.createShader(gl.FRAGMENT_SHADER);
-            gl.shaderSource(fs, fsSouce);
-            gl.compileShader(fs);
-
-            // Link together into a program
-            var prog = gl.createProgram();
-            gl.attachShader(prog, vs);
-            gl.attachShader(prog, fs);
-            gl.linkProgram(prog);
-            gl.useProgram(prog);
-
-            // Load vertex data into a buffer
-            var vertices = new Float32Array([1, 1, -1, 1, 1, -1, -1, -1]);
-            var vbo = gl.createBuffer();
-            gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-            gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-
-            // Bind vertex buffer to attribute variable
-            var posAttrib = gl.getAttribLocation(prog, 'pos');
-            gl.vertexAttribPointer(posAttrib, 2, gl.FLOAT, false, 0, 0);
-            gl.enableVertexAttribArray(posAttrib);
-
-            // Clear framebuffer and render primitives
-            var vsSource = 'attribute vec2 pos;'+
-                'void main(){gl_Position = vec4(pos * 0.5, 0, 1); }';
-            var vs = gl.createShader(gl.VERTEX_SHADER);
-            gl.shaderSource(vs, vsSource);
-            gl.compileShader(vs);
-
-            // Compile a fragment shader
-            fsSouce =  'void main() { gl_FragColor = vec4(1); }';
-            var fs = gl.createShader(gl.FRAGMENT_SHADER);
-            gl.shaderSource(fs, fsSouce);
-            gl.compileShader(fs);
-
-            // Link together into a program
-            var prog = gl.createProgram();
-            gl.attachShader(prog, vs);
-            gl.attachShader(prog, fs);
-            gl.linkProgram(prog);
-            gl.useProgram(prog);
-
-            // Load vertex data into a buffer
-            var vertices = new Float32Array([1, 1, -1, 1, 1, -1, -1, -1]);
-            var vbo = gl.createBuffer();
-            gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-            gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-
-            // Bind vertex buffer to attribute variable
-            var posAttrib = gl.getAttribLocation(prog, 'pos');
-            gl.vertexAttribPointer(posAttrib, 2, gl.FLOAT, false, 0, 0);
-            gl.enableVertexAttribArray(posAttrib);
-
-            // Clear framebuffer and render primitives
-            var vsSource = 'attribute vec2 pos;'+
-                'void main(){gl_Position = vec4(pos * 0.5, 0, 1); }';
-            var vs = gl.createShader(gl.VERTEX_SHADER);
-            gl.shaderSource(vs, vsSource);
-            gl.compileShader(vs);
-
-            // Compile a fragment shader
-            fsSouce =  'void main() { gl_FragColor = vec4(1); }';
-            var fs = gl.createShader(gl.FRAGMENT_SHADER);
-            gl.shaderSource(fs, fsSouce);
-            gl.compileShader(fs);
-
-            // Link together into a program
-            var prog = gl.createProgram();
-            gl.attachShader(prog, vs);
-            gl.attachShader(prog, fs);
-            gl.linkProgram(prog);
-            gl.useProgram(prog);
-
-            // Load vertex data into a buffer
-            var vertices = new Float32Array([1, 1, -1, 1, 1, -1, -1, -1]);
-            var vbo = gl.createBuffer();
-            gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-            gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-
-            // Bind vertex buffer to attribute variable
-            var posAttrib = gl.getAttribLocation(prog, 'pos');
-            gl.vertexAttribPointer(posAttrib, 2, gl.FLOAT, false, 0, 0);
-            gl.enableVertexAttribArray(posAttrib);
-
-            // Clear framebuffer and render primitives
-            var vsSource = 'attribute vec2 pos;'+
-                'void main(){gl_Position = vec4(pos * 0.5, 0, 1); }';
-            var vs = gl.createShader(gl.VERTEX_SHADER);
-            gl.shaderSource(vs, vsSource);
-            gl.compileShader(vs);
-
-            // Compile a fragment shader
-            fsSouce =  'void main() { gl_FragColor = vec4(1); }';
-            var fs = gl.createShader(gl.FRAGMENT_SHADER);
-            gl.shaderSource(fs, fsSouce);
-            gl.compileShader(fs);
-
-            // Link together into a program
-            var prog = gl.createProgram();
-            gl.attachShader(prog, vs);
-            gl.attachShader(prog, fs);
-            gl.linkProgram(prog);
-            gl.useProgram(prog);
-
-            // Load vertex data into a buffer
-            var vertices = new Float32Array([1, 1, -1, 1, 1, -1, -1, -1]);
-            var vbo = gl.createBuffer();
-            gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-            gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-
-            // Bind vertex buffer to attribute variable
-            var posAttrib = gl.getAttribLocation(prog, 'pos');
-            gl.vertexAttribPointer(posAttrib, 2, gl.FLOAT, false, 0, 0);
-            gl.enableVertexAttribArray(posAttrib);
-
-            // Clear framebuffer and render primitives
-            var vsSource = 'attribute vec2 pos;'+
-                'void main(){gl_Position = vec4(pos * 0.5, 0, 1); }';
-            var vs = gl.createShader(gl.VERTEX_SHADER);
-            gl.shaderSource(vs, vsSource);
-            gl.compileShader(vs);
-
-            // Compile a fragment shader
-            fsSouce =  'void main() { gl_FragColor = vec4(1); }';
-            var fs = gl.createShader(gl.FRAGMENT_SHADER);
-            gl.shaderSource(fs, fsSouce);
-            gl.compileShader(fs);
-
-            // Link together into a program
-            var prog = gl.createProgram();
-            gl.attachShader(prog, vs);
-            gl.attachShader(prog, fs);
-            gl.linkProgram(prog);
-            gl.useProgram(prog);
-
-            // Load vertex data into a buffer
-            var vertices = new Float32Array([1, 1, -1, 1, 1, -1, -1, -1]);
-            var vbo = gl.createBuffer();
-            gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-            gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-
-            // Bind vertex buffer to attribute variable
-            var posAttrib = gl.getAttribLocation(prog, 'pos');
-            gl.vertexAttribPointer(posAttrib, 2, gl.FLOAT, false, 0, 0);
-            gl.enableVertexAttribArray(posAttrib);
-
-            // Clear framebuffer and render primitives
-            var vsSource = 'attribute vec2 pos;'+
-                'void main(){gl_Position = vec4(pos * 0.5, 0, 1); }';
-            var vs = gl.createShader(gl.VERTEX_SHADER);
-            gl.shaderSource(vs, vsSource);
-            gl.compileShader(vs);
-
-            // Compile a fragment shader
-            fsSouce =  'void main() { gl_FragColor = vec4(1); }';
-            var fs = gl.createShader(gl.FRAGMENT_SHADER);
-            gl.shaderSource(fs, fsSouce);
-            gl.compileShader(fs);
-
-            // Link together into a program
-            var prog = gl.createProgram();
-            gl.attachShader(prog, vs);
-            gl.attachShader(prog, fs);
-            gl.linkProgram(prog);
-            gl.useProgram(prog);
-
-            // Load vertex data into a buffer
-            var vertices = new Float32Array([1, 1, -1, 1, 1, -1, -1, -1]);
-            var vbo = gl.createBuffer();
-            gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-            gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-
-            // Bind vertex buffer to attribute variable
-            var posAttrib = gl.getAttribLocation(prog, 'pos');
-            gl.vertexAttribPointer(posAttrib, 2, gl.FLOAT, false, 0, 0);
-            gl.enableVertexAttribArray(posAttrib);
-
-            // Clear framebuffer and render primitives
-            var vsSource = 'attribute vec2 pos;'+
-                'void main(){gl_Position = vec4(pos * 0.5, 0, 1); }';
-            var vs = gl.createShader(gl.VERTEX_SHADER);
-            gl.shaderSource(vs, vsSource);
-            gl.compileShader(vs);
-
-            // Compile a fragment shader
-            fsSouce =  'void main() { gl_FragColor = vec4(1); }';
-            var fs = gl.createShader(gl.FRAGMENT_SHADER);
-            gl.shaderSource(fs, fsSouce);
-            gl.compileShader(fs);
-
-            // Link together into a program
-            var prog = gl.createProgram();
-            gl.attachShader(prog, vs);
-            gl.attachShader(prog, fs);
-            gl.linkProgram(prog);
-            gl.useProgram(prog);
-
-            // Load vertex data into a buffer
-            var vertices = new Float32Array([1, 1, -1, 1, 1, -1, -1, -1]);
-            var vbo = gl.createBuffer();
-            gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-            gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-
-            // Bind vertex buffer to attribute variable
-            var posAttrib = gl.getAttribLocation(prog, 'pos');
-            gl.vertexAttribPointer(posAttrib, 2, gl.FLOAT, false, 0, 0);
-            gl.enableVertexAttribArray(posAttrib);
-
-            // Clear framebuffer and render primitives
-            gl.clear(gl.COLOR_BUFFER_BIT);
-            gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+clearContextAndDraw(gl)
